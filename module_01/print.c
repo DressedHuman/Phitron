3f86249dda2cc37d52e36e883c3744dc29452c98
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-09-22 23:50:23 +0600

    wrote first program - print.c

---
+++ module_01/print.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello world\tthere is a tab before");
+    printf("Now I am a programmer\n");
+    printf("This line prints a backslash: \\\n");
+    printf("This line prints a percentage sign: %%");
+    return 0;
+}
+
+// Hello world
+// Now I am a programmer
+// This line prints a backslash: \
+// This line prints a percentage sign: %
