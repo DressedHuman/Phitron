f7252eb10397bcacd28baf1f2ad22eb5f52590f2
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-09-23 17:30:30 +0600

    learned conditional operation

---
+++ module_02/conditional_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if(tk>=100){
+        printf("Burgar khabo");
+    }
+    else{
+        printf("Saradin kichu khabo na!");
+    }
+    return 0;
+}
