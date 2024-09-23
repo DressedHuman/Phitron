3693447d58e70be8da1459dc51f1f318189d3db1
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-09-23 09:15:50 +0600

    learned limitations of data types

---
+++ module_01/limitations.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=2000000000;
+    long long b=9223372030000000000;
+    float f=75057.577557;
+    double d=2122.123456789123456;
+    printf("%0.15lf", d);
+    return 0;
+}
